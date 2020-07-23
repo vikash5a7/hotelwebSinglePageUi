d3f7cd5da133b439a0d237229dc475e95f3dd413
vikash kumar <vikashkumargupta5a7@gmail.com>
2020-07-24 01:30:27 +0530

    [vikash] complted with animation and trasition

--- main.js
+++ main.js
@@ -7,5 +7,32 @@ let menuToggler = selectElement('.menu-toggle');
 let body = selectElement('body');
 
 menuToggler.addEventListener('click', function() {
-    body.classList.toggle('open');
-})
+        body.classList.toggle('open');
+    })
+    // scroll revel
+window.sr = ScrollReveal();
+sr.reveal('.animate-left', {
+    origin: 'left',
+    duration: 1000,
+    distance: '25rem',
+    delay: 300
+});
+sr.reveal('.animate-right', {
+    origin: 'right',
+    duration: 1000,
+    distance: '25rem',
+    delay: 300
+});
+
+sr.reveal('.animate-top', {
+    origin: 'top',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
+});
+sr.reveal('.animate-bottom', {
+    origin: 'bottom',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
+});
